06c2b0c1f93a101471b9032121a3b94ff549a70b
ksn-crafter <kuldeep@caizin.com>
2025-03-24 19:00:17 +0530

    refactor: unearth feature envy smell

--- Java/src/main/java/com/gildedrose/GildedRose.java
+++ Java/src/main/java/com/gildedrose/GildedRose.java
@@ -10,7 +10,7 @@ class GildedRose {
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].isAgedBrie()
-                    && !isBackstagePasses(i)) {
+                    && !isBackstagePasses(items[i])) {
                 if (items[i].quality > 0) {
                     if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                         items[i].quality = items[i].quality - 1;
@@ -20,7 +20,7 @@ class GildedRose {
                 if (items[i].quality < 50) {
                     items[i].quality = items[i].quality + 1;
 
-                    if (isBackstagePasses(i)) {
+                    if (isBackstagePasses(items[i])) {
                         if (items[i].sellIn < 11) {
                             if (items[i].quality < 50) {
                                 items[i].quality = items[i].quality + 1;
@@ -42,7 +42,7 @@ class GildedRose {
 
             if (items[i].sellIn < 0) {
                 if (!items[i].isAgedBrie()) {
-                    if (!isBackstagePasses(i)) {
+                    if (!isBackstagePasses(items[i])) {
                         if (items[i].quality > 0) {
                             if (!items[i].name.equals("Sulfuras, Hand of Ragnaros")) {
                                 items[i].quality = items[i].quality - 1;
@@ -60,8 +60,8 @@ class GildedRose {
         }
     }
 
-    private boolean isBackstagePasses(int i) {
-        return items[i].name.equals("Backstage passes to a TAFKAL80ETC concert");
+    private static boolean isBackstagePasses(Item item) {
+        return item.name.equals("Backstage passes to a TAFKAL80ETC concert");
     }
 
 }
