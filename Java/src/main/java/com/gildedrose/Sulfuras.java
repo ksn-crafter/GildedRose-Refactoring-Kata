576bb8dfdd7d790f6889c4424134c01eb51df2a6
ksn-crafter <kuldeep@caizin.com>
2025-03-25 15:26:45 +0530

    refactor: implement updateQuality for Sulfuras

--- Java/src/main/java/com/gildedrose/Sulfuras.java
+++ Java/src/main/java/com/gildedrose/Sulfuras.java
@@ -4,4 +4,14 @@ public class Sulfuras extends Item {
     public Sulfuras(int sellIn, int quality) {
         super("Sulfuras, Hand of Ragnaros", sellIn, quality);
     }
+
+    @Override
+    public void updateQuality() {
+        decreaseQuality();
+        decreaseSellIn();
+
+        if (isSellInLessThan(0)) {
+            decreaseQuality();
+        }
+    }
 }
