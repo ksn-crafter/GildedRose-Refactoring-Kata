b14165b4ac8d14b749e4be0b01af1d5cade1ec53
ksn-crafter <kuldeep@caizin.com>
2025-03-25 15:58:17 +0530

    refactor: replace magic number with constants

--- Java/src/main/java/com/gildedrose/BackstagePasses.java
+++ Java/src/main/java/com/gildedrose/BackstagePasses.java
@@ -1,6 +1,10 @@
 package com.gildedrose;
 
 public class BackstagePasses extends Item {
+
+    public static final int INCREASE_QUALITY_BY_TWO_FOR_SELLIN_LIMIT = 11;
+    public static final int INCREASE_QUALITY_BY_THREE_FOR_SELLIN_LIMIT = 6;
+
     public BackstagePasses(String eventName, int sellIn, int quality) {
         super(String.format("Backstage passes to a %s", eventName), sellIn, quality);
     }
@@ -9,11 +13,11 @@ public class BackstagePasses extends Item {
     public void increementQuality(){
         super.increementQuality();
 
-        if (isSellInLessThan(11) && isQualityLessThanMaxPermissibleQuality()) {
+        if (isSellInLessThan(INCREASE_QUALITY_BY_TWO_FOR_SELLIN_LIMIT) && isQualityLessThanMaxPermissibleQuality()) {
             super.increementQuality();
         }
 
-        if (isSellInLessThan(6) && isQualityLessThanMaxPermissibleQuality()) {
+        if (isSellInLessThan(INCREASE_QUALITY_BY_THREE_FOR_SELLIN_LIMIT) && isQualityLessThanMaxPermissibleQuality()) {
             super.increementQuality();
         }
     }
