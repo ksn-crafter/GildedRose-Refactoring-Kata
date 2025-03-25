383ffe3b844ffc879253aa8407f9bffadedfad85
ksn-crafter <kuldeep@caizin.com>
2025-03-25 12:53:10 +0530

    refactor: add specialized item types

---
+++ Java/src/main/java/com/gildedrose/AgedBrie.java
@@ -0,0 +1,7 @@
+package com.gildedrose;
+
+public class AgedBrie extends Item{
+    public AgedBrie(String name, int sellIn, int quality) {
+        super(name,sellIn,quality);
+    }
+}
---
+++ Java/src/main/java/com/gildedrose/NormalItem.java
@@ -0,0 +1,7 @@
+package com.gildedrose;
+
+public class NormalItem extends Item{
+    public NormalItem(String name, int sellIn, int quality) {
+       super(name,sellIn,quality);
+    }
+}
